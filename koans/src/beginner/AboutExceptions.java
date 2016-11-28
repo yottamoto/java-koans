3ead95427ac56f087d0bee7b27d00a639937ff49
Christian Capito <chris.capito@gmail.com>
2016-11-28 23:23:37 +0100

    ... And AboutExceptions is done

--- koans/src/beginner/AboutExceptions.java
+++ koans/src/beginner/AboutExceptions.java
@@ -12,7 +12,7 @@ public class AboutExceptions {
 		throw new IOException();
 	}
 	
-	@Koan
+	@Koan 
 	public void catchCheckedExceptions() {
 		String s;
 		try {
@@ -21,7 +21,7 @@ public class AboutExceptions {
 		} catch(IOException e) { 
 			s = "exception thrown";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "exception thrown");
 	}
 	
 	@Koan
@@ -35,7 +35,7 @@ public class AboutExceptions {
 		} finally {
 			s += " and finally ran as well";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "exception thrown and finally ran as well");
 	}
 	
 	@Koan
@@ -46,7 +46,7 @@ public class AboutExceptions {
 		} finally {
 			s += " and finally ran as well";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "code ran normally and finally ran as well");
 	}
 	
 	private void tryCatchFinallyWithVoidReturn(StringBuilder whatHappened) {
@@ -65,7 +65,7 @@ public class AboutExceptions {
 	public void finallyIsAlwaysRan() {
 		StringBuilder whatHappened = new StringBuilder();
 		tryCatchFinallyWithVoidReturn(whatHappened);
-		assertEquals(whatHappened.toString(), __);
+		assertEquals(whatHappened.toString(), "did something dangerous; the catch block executed, but so did the finally!");
 	}
 	
 	@SuppressWarnings("finally") // this is suppressed because returning in finally block is obviously a compiler warning
@@ -89,8 +89,8 @@ public class AboutExceptions {
 	public void returnInFinallyBlock() {
 		StringBuilder whatHappened = new StringBuilder();
 		// Which value will be returned here?
-		assertEquals(returnStatementsEverywhere(whatHappened), __);
-		assertEquals(whatHappened.toString(), __);
+		assertEquals(returnStatementsEverywhere(whatHappened), "from finally");
+		assertEquals(whatHappened.toString(), "try, catch, finally" );
 	}
 	
 	private void doUncheckedStuff() {
@@ -100,7 +100,11 @@ public class AboutExceptions {
 	@Koan
 	public void catchUncheckedExceptions() {
 		// What do you need to do to catch the unchecked exception?
-		doUncheckedStuff();
+		try {
+			doUncheckedStuff();			
+		} catch (Exception e) {
+			// just catch it?
+		}
 	}
 	
 	@SuppressWarnings("serial")
@@ -122,7 +126,7 @@ public class AboutExceptions {
 		} catch(ParentException e) {
 			s = "ParentException";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "ChildException");
 	}	
 
 	@Koan
@@ -134,7 +138,7 @@ public class AboutExceptions {
 		} catch (IllegalArgumentException ex) {
 			s = "caught an IllegalArgumentException";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "caught an IllegalArgumentException");
 	}
 	
 	@Koan
@@ -146,7 +150,7 @@ public class AboutExceptions {
 		} catch (IllegalArgumentException ex) {
 			s = "caught an IllegalArgumentException";
 		}
-		assertEquals(s, __);
+		assertEquals(s, "5");
 	}
 
 	private int validateUsingIllegalArgumentException(String str) {
